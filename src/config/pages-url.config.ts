7e43e597d2bbe8f09c575e05e43df4573227fafe
t1tandr <titandr18@gmail.com>
2025-03-11 18:09:42 +0200

    basic settings and packages

---
+++ src/config/pages-url.config.ts
@@ -0,0 +1,8 @@
+class PAGES {
+  static readonly HOME = '/'
+  static readonly LOGIN = '/login'
+  static readonly REGISTER = '/register'
+  static readonly PROFILE = '/profile'
+  static readonly CALENDAR = '/calendar'
+}
+export const PAGES_URL = new PAGES()
